aef711a35c4183e0ae4881cf38f83a896a2f9f71
ckugel <calebkugel1@gmail.com>
2024-11-12 13:01:58 -0600

    started cliff sensor file def

---
+++ EmbeddedApp/Waste_Management/cliff.c
@@ -0,0 +1,32 @@
+/*
+ * cliff.c
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: ckugel05
+ */
+
+bool hole_Fleft() {
+
+}
+
+bool hole_Fright() {
+
+}
+
+bool hole_left() {
+
+}
+
+bool hole_right() {
+
+}
+
+bool target_left() {
+
+}
+
+bool target_right() {
+
+}
+
+
---
+++ EmbeddedApp/Waste_Management/cliff.h
@@ -0,0 +1,27 @@
+/*
+ * cliff.h
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: ckugel05
+ */
+
+#ifndef CLIFF_H_
+#define CLIFF_H_
+
+#include "open_interface.h"
+
+
+bool hole_Fleft();
+
+bool hole_Fright();
+
+bool hole_left();
+
+bool hole_right();
+
+bool target_left();
+
+bool target_right();
+
+
+#endif /* CLIFF_H_ */
