17affab9b583f11584466e61848f42b71ab61353
Udip Shrestha <72911932+udip-shrestha@users.noreply.github.com>
2024-12-03 10:41:45 -0600

    Update imu.h

--- EmbeddedApp/Waste_Management/imu.h
+++ EmbeddedApp/Waste_Management/imu.h
@@ -1,5 +1,13 @@
+#ifndef IMU_H_
+#define IMU_H_
+
+#include <stdint.h>
+
+// Function declarations
 void imu_init();
 void imu_writeRegister(uint8_t reg, uint8_t value);
 uint8_t imu_readRegister(uint8_t reg);
 uint16_t imu_getHeading();
 uint8_t imu_getCalibrationStatus();
+
+#endif /* IMU_H_ */
