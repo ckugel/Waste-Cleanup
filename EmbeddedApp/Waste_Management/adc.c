97cbafb980e0db0167981894a884c85b63d78bd2
Udip Shrestha <72911932+udip-shrestha@users.noreply.github.com>
2024-12-09 19:07:52 -0600

    Added comments

--- EmbeddedApp/Waste_Management/adc.c
+++ EmbeddedApp/Waste_Management/adc.c
@@ -9,10 +9,10 @@
 
 void adc_init(){
 
-    SYSCTL_RCGCADC_R |= 0x1;
-    SYSCTL_RCGCGPIO_R |= 0x2;
+    SYSCTL_RCGCADC_R |= 0x1;    // Enable the ADC0 module by setting bit 0 in the ADC run mode clock gating control register.
+    SYSCTL_RCGCGPIO_R |= 0x2;    // Enable the clock for GPIO Port B by setting bit 1 in the GPIO clock gating register.
 
-    GPIO_PORTB_AFSEL_R |= 0x10;
+    GPIO_PORTB_AFSEL_R |= 0x10;    // Enable the alternate function 
 
     GPIO_PORTB_DIR_R   &= ~0x10;
     GPIO_PORTB_AMSEL_R |= 0x10;
@@ -27,7 +27,13 @@ void adc_init(){
     ADC0_ACTSS_R |= 0x1;
     ADC0_PSSI_R |= 0x1;
 }
-
+/**
+ * Reads the ADC value from the configured channel.
+ *
+ * Starts a conversion and waits for the result to be available.
+ *
+ * @return int The ADC conversion result.
+ */
 int adc_read(){
     ADC0_PSSI_R |= 0x1;
     int adc_output = ADC0_SSFIFO0_R;
